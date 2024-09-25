17a09bb4dc1ef8c0c05281c0006524badabc5b20
Héctor <hff150305@gmail.com>
2024-09-25 13:52:52 +0200

    a

--- Assets/Scripts/CardDatabase.cs
+++ Assets/Scripts/CardDatabase.cs
@@ -17,7 +17,7 @@ public class CardDatabase : MonoBehaviour
         cardList.Add(new Card(4, "Phaksa", 1, 2, 2, 3, "Una aventurera demi-humana olvidada por el mundo", "N", "", 0));
         cardList.Add(new Card(5, "Daro", 1, 1, 1, 6, "Un aventurero enano olvidado por el mundo", "N", "", 0));
         // Mundo Antiguo (Antes de los dioses (Desbloqueo))
-        // cardList.Add(new Card(1000, "Blight", 10, 20, 20, 20, "Un rey olvidado por el mundo", "N", -1000, 0));
+        
         // Mundo Antiguo 2 (Nº)
         cardList.Add(new Card(6, "Nº12 ", 1, 3, 0, 3, "Al ser destruida invoca de forma especial desde tu mano o deck a Nº11", "N", "", 0));
         cardList.Add(new Card(7, "Nº11", 2, 1, 4, 4, "Al ser invocada de forma especial crea 2 fichas [soldado de arena] 0/4/1, al ser destruida invoca de forma especial desde tu mano o deck a Nº10 ", "none", "", 0));
@@ -32,12 +32,12 @@ public class CardDatabase : MonoBehaviour
         cardList.Add(new Card(16, "Nº2", 11, 11, 10, 9, "Al ser invocada de forma especial busca una carta mágica o trampa Nº y añadela a tu mano, al morir invoca de forma especial desde tu mano o deck a Nº2", "none", "", 0));
         cardList.Add(new Card(17, "Nº1", 12, 12, 12, 12, "Al ser invocada de forma especial gasta todo tu mana, esta carta no puede ser seleccionada por efectos, al final de cada ronda recupera toda su vida", "none", "Guerrera", 0));
 
-        cardList.Add(new Card(18, "Último esfuerzo Nº", 0, 0, 0, 0, "Destruye un elegido Nº después destruye un elegido del rival, después de activar el efecto no puedes invocar elegidos que no sean Nº ", "Mágica", "", 0));
-        cardList.Add(new Card(19, "Retirada Nº", 0, 0, 0, 0, "Devuelve al Deck cualquier número de Nº, roba una carta por cada una", "Mágica", "", 0));
-        cardList.Add(new Card(20, "Regrso Nº", 0, 0, 0, 0, "Cuando una elegida Nº es destruida, envia al deck 1 elegida Nº", "Trampa", "", 0));
-        cardList.Add(new Card(21, "Plan de contención Nº", 0, 0, 0, 0, "Cuando una elegida Nº es destruida, envia al deck del cementerio 1 elegida Nº", "Trampa", "", 0));
-        // Mundo Antiguo 2 (Nº (Desbloqueo))
-        // cardList.Add(new Card(1001, "Nº0", 1, 1, 1, 1, "Cuando esta carta es invocada destierra todas las cartas Nº aumenta en 1 su vida por cada una", "N", -480, 0));
+        cardList.Add(new Card(18, "Último esfuerzo Nº", 2, 0, 0, 0, "Destruye un elegido Nº después destruye un elegido del rival, después de activar el efecto no puedes invocar elegidos que no sean Nº ", "Mágica", "", 0));
+        cardList.Add(new Card(19, "Retirada Nº",1, 0, 0, 0, "Devuelve al Deck cualquier número de Nº, roba una carta por cada una", "Mágica", "", 0));
+        cardList.Add(new Card(20, "Regrso Nº", 4, 0, 0, 0, "Cuando una elegida Nº es destruida, invoca del cementerio una Nº distinta a la que se acaba de enviar", "Trampa", "", 0));
+        cardList.Add(new Card(21, "Plan de contención Nº", 1, 0, 0, 0, "Cuando una elegida Nº es destruida, envia al deck del cementerio 1 elegida Nº", "Trampa", "", 0));
+    
+        
 
         // Mundo Antiguo 3 (Defensores del futuro)
         cardList.Add(new Card(22, "Lyriel", 2, 4, 2, 3, "Puede atacar directamente al rival", "N", "Pícara", 0));
@@ -66,11 +66,11 @@ public class CardDatabase : MonoBehaviour
         // Mundo Antiguo 5
         cardList.Add(new Card(40, "Rana Mercante", 1, 0, 0, 1, "Una vez por ronda puedes descartar 2 cartas para robar 1", "none", "Mercader", 0));
         
-        cardList.Add(new Card(41, "Reglas antiguas", 0, 0, 0, 0, "Elimina todos los manas de los dioses", "Mágica", "", 0));
+        cardList.Add(new Card(41, "Reglas antiguas", 2, 0, 0, 0, "Elimina todos los manas de los dioses", "Mágica", "", 0));
         cardList.Add(new Card(42, "Último recurso", 0, 0, 0, 0, "Si tu vida es 1, obten 20 de mana", "Mágica", "", 0));
-        cardList.Add(new Card(43, "Vuelta a lo básico", 0, 0, 0, 0, "Transforma todo el mana de los dioses a mana", "Mágica", "", 0));
-        cardList.Add(new Card(44, "Último esfuerzo", 0, 0, 0, 0, "Cuando un elegido vaya a ser destruido mantenlo a 1 de vida", "Trampa", ""   , 0));
-        cardList.Add(new Card(45, "Castigo divino", 0, 0, 0, 0, "Destruye todos los monstruos en el campo", "Mágica", "", 0));
+        cardList.Add(new Card(43, "Vuelta a lo básico", 1, 0, 0, 0, "Transforma todo el mana de los dioses a mana", "Mágica", "", 0));
+        cardList.Add(new Card(44, "Último esfuerzo", 1, 0, 0, 0, "Cuando un elegido vaya a ser destruido mantenlo a 1 de vida", "Trampa", ""   , 0));
+        cardList.Add(new Card(45, "Castigo divino", 4, 0, 0, 0, "Destruye todos los monstruos en el campo", "Mágica", "", 0));
 
 
         // Revolución Steampunk (La creacion de K.R.I.S)
@@ -82,22 +82,22 @@ public class CardDatabase : MonoBehaviour
         cardList.Add(new Card(51, "Nora Bolt", 6, 5, 2, 6, "Al ser invocada si Kris Bolt o K.R.I.S estan en el campo puedes buscar una carta que mencione a uno de los dos en tu deck y añadirla a tu mano ", "Miknit", "Artificiera", 0));
         cardList.Add(new Card(51, "K.R.I.S", 10, 10, 10, 10, "Si esta en posición de defensa los elegidos enemigos solo pueden atacarle a el", "N", "Robot", 0));
         
-        cardList.Add(new Card(52, "Electric field", 0, 0, 0, 0, "Aumenta el ataque de todos los elegidos Bolt y K.R.I.S en 1, si no hay al menos 1 Bolt en campo al finalizar el turno es destruida", "Campo", "", 0));
-        cardList.Add(new Card(53, "Plan Bolt", 0, 0, 0, 0, "Si tienes 1 Bolt en el campo, 1 en el cementerio y 1 en la mano puedes invocar a K.R.I.S", "Mágica", "", 0));
-        cardList.Add(new Card(54, "Eureka!", 0, 0, 0, 0, "Puedes equipar hasta 6 elegidos Bolt con nombres diferentes a K.R.I.S, este gana 1 de defensa y 2 de vida por cada uno", "Mágica", "", 0));
+        cardList.Add(new Card(52, "Electric field", 1, 0, 0, 0, "Aumenta el ataque de todos los elegidos Bolt y K.R.I.S en 1, si no hay al menos 1 Bolt en campo al finalizar el turno es destruida", "Dominio", "", 0));
+        cardList.Add(new Card(53, "Plan Bolt", 3, 0, 0, 0, "Si tienes 1 Bolt en el campo, 1 en el cementerio y 1 en la mano puedes invocar a K.R.I.S", "Mágica", "", 0));
+        cardList.Add(new Card(54, "Eureka!", 5, 0, 0, 0, "Puedes equipar hasta 6 elegidos Bolt con nombres diferentes a K.R.I.S, este gana 1 de defensa y 2 de vida por cada uno", "Mágica", "", 0));
         // Revolución Steampunk 2 (Mecánica Fantástica y Artefactos Extaordianrios)
         cardList.Add(new Card(55, "Kris Bolt", 5, 8, 1, 3, "Cuando es tu turno duplica su ataque, cuando es el turno del rival reducelo a la mitad", "Miknit", "Artificiero", 0));
         cardList.Add(new Card(56, "Valette Bleck", 4, 5, 2, 2, "Al ser invocada roba una carta de equipo, puedes destruir esta para destruir un elegido del rival", "Chronos", "Tiradora", 0));
         cardList.Add(new Card(57, "Nara", 3, 1, 2, 8, "Puede tener hasta 8 cartas equipadas", "Dana", "Mercenaria", 0));
         cardList.Add(new Card(58, "B.S.B-G.A.P-103", 8, 8, 16, 12, "Cada vez que destruye un elegido rival aumenta en 1 todas sus estadísticas", "N", "Robot", 0));
 
-        cardList.Add(new Card(59, "Bateria de Atem", 0, 0, 0, 0, "El elegido equipado con esto puede atacar dos veces por turno", "Equipo", "", 0));
-        cardList.Add(new Card(60, "Lia", 0, 0, 0, 0, "Disminuye en 1 el ataque, cuando esta carta es destruida invoca en el campo del poseedor de la carta una ficha Lia 9/0/1", "Equipo", "", 0));
-        cardList.Add(new Card(61, "Pistola Bleck", 0, 0, 0, 0, "Cuando el elegido que la lleve equipada ataque inflige 1 de daño a tu oponente", "Equipo", "", 0));
-        cardList.Add(new Card(62, "Rifle Keiser", 0, 0, 0, 0, "Aumenta en 1 el ataque, si esta carta es enviada al cementerio destruye un elegido", "Equipo", "", 0));
-        cardList.Add(new Card(63, "EléctroBola", 0, 0, 0, 0, "Solo puede ser equipada en elegidos bolt, duplica el ataque de estos cuando atacan a un elegido que no sea de Miknit", "Equipo", "", 0));
+        cardList.Add(new Card(59, "Bateria de Atem", 4, 0, 0, 0, "El elegido equipado con esto puede atacar dos veces por turno", "Equipo", "", 0));
+        cardList.Add(new Card(60, "Lia", 4, 0, 0, 0, "Disminuye en 1 el ataque, cuando esta carta es destruida invoca en el campo del poseedor de la carta una ficha Lia 9/0/1", "Equipo", "", 0));
+        cardList.Add(new Card(61, "Pistola Bleck", 2, 0, 0, 0, "Cuando el elegido que la lleve equipada ataque inflige 1 de daño a tu oponente", "Equipo", "", 0));
+        cardList.Add(new Card(62, "Rifle Keiser", 3, 0, 0, 0, "Aumenta en 1 el ataque, si esta carta es enviada al cementerio destruye un elegido", "Equipo", "", 0));
+        cardList.Add(new Card(63, "EléctroBola", 3, 0, 0, 0, "Solo puede ser equipada en elegidos bolt, duplica el ataque de estos cuando atacan a un elegido que no sea de Miknit", "Equipo", "", 0));
         // Revolución Steampunk 2 (Mecánica Fantástica y Artefactos Extaordianrios)
-        // cardList.Add(new Card(1002, "HelAI", 6, 3, 14, 5, "Si este elegido es el único en tu campo es destruido, si un elegido tuyo tiene una carta de equipo puedes invocar a este de forma especial", "Chronos", 0, 0));
+        // 
 
         // Revolución Steampunk 3 (Revolucionarios)
         cardList.Add(new Card(64, "Sonya Delay", 10, 1, 10, 10, "Mientras esta carta este en el campo tu rival no puede atacar con sus elegidos", "Chronos", "", 0));
@@ -108,20 +108,50 @@ public class CardDatabase : MonoBehaviour
         cardList.Add(new Card(69, "Mona", 10, 10, 10, 10, "Cuando es invocada destruye todos los elegidos que su mana no sea de Etse", "Etse", "", 0));
 
         cardList.Add(new Card(70, "Deicidio", 10, 0, 0, 0, "Si todo tu mana es mana de los dioses transformalo en mana, aumenta permanentemente en 2 tu mana maximo, no puedes obtener mana de los dioses después de activar este efecto", "none", "", 0));
-
+        
+        cardList.Add(new Card(71, "Emperor Red", 6, 0, 0, 0, "Siempre que un elegido de chronos ataque el elegido del rival no puede contraatacar", "Dominio", "", 0));
+        cardList.Add(new Card(72, "Baño de sangre", 6, 0, 0, 0, "Cuando un elegido es destruido recupera toda la vida de tus elegidos de murgu", "Dominio", "", 0));
+        cardList.Add(new Card(73, "Electromagnetismo", 6, 0, 0, 0, "Al invocar un elegido de Miknit puede atacar sin esperar", "Dominio", "", 0));
+        cardList.Add(new Card(74, "Neblina púrpura", 8, 0, 0, 0, "Los elegidos invocados que no sean de Dana pierden la mitad de su ataque y defensa", "Dominio", "", 0));
+        cardList.Add(new Card(75, "Zona extraña", 1, 0, 0, 0, "Cambia el ataque por la defensa de todos los elegidos invocados, si el elegido es de Yrys el invocador puede decir si cambiar o no el ataque", "Dominio", "", 0));
+        cardList.Add(new Card(76, "Cementerio de almas", 6, 0, 0, 0, "Cuando un elegido es destruido traelo de vuelta desde el cementerio, la próxima vez que fueran a ser destruidos destierralos", "Dominio", "", 0));
         // Revolución Steampunk 4 (Linea de defensa)
-        cardList.Add(new Card(71, "Ruby VIII", 5, 2, 2, 5, "Cuando es invocada roba una carta mágica a eleccion", "Miknit", "", 0));
-        cardList.Add(new Card(72, "Eins", 5, 10, 1, 1, "Cuando es invocado de forma normal invoca desde el deck o mano a Zwei y Drei", "Chronos", "Artificiero", 0));
-        cardList.Add(new Card(73, "Zwei", 5, 10, 1, 1, "Cuando es invocado de forma normal invoca desde el deck o mano a Eins y Drei", "Chronos", "Artificiero", 0));
-        cardList.Add(new Card(74, "Drei", 5, 10, 1, 1, "Cuando es invocado de forma normal invoca desde el deck o mano a Eins y Zwei", "Chronos", "Artificiero", 0));
+        cardList.Add(new Card(77, "Ruby VIII", 5, 2, 2, 5, "Cuando es invocada roba una carta mágica a eleccion", "Miknit", "", 0));
+        cardList.Add(new Card(78, "Eins", 5, 10, 1, 1, "Cuando es invocado de forma normal invoca desde el deck o mano a Zwei y Drei", "Chronos", "Artificiero", 0));
+        cardList.Add(new Card(79, "Zwei", 5, 10, 1, 1, "Cuando es invocado de forma normal invoca desde el deck o mano a Eins y Drei", "Chronos", "Artificiero", 0));
+        cardList.Add(new Card(80, "Drei", 5, 10, 1, 1, "Cuando es invocado de forma normal invoca desde el deck o mano a Eins y Zwei", "Chronos", "Artificiero", 0));
+        cardList.Add(new Card(81, "Atom", 5, 6, 5, 7, "Al ser invocada destruye una carta de tu adversario", "Dana", "Maga", 0));
+        cardList.Add(new Card(82, "Vix", 5, 7, 3, 5, "Al ser invocada equipala a un monstruo rival este pierde todo su ataque, cuando este es destruido devuelve esta carta a tu zona de elegidos", "none", "", 0));
 
-        cardList.Add(new Card(0, "none", 0, 0, 0, 0, "none", "none", "", 0));
+        // Revolución Steampunk 5 (Nuevos Trabajos)
+        cardList.Add(new Card(83, "Myu", 2, 1, 1, 1, "Cuando es invocada busca un elegido en el deck del rival, destierralo", "Chronos", "Tirador", 0));
+        cardList.Add(new Card(84, "Drip", 2, 1, 1, 1, "Cuando es invocada busca una mágica en el deck del rival, destierrala", "Dana", "Mercader", 0));
+        cardList.Add(new Card(85, "BeeBot", 2, 1, 1, 1, "Cuando es invocada roba una mágica en el deck", "N", "Robot", 0));
+        cardList.Add(new Card(86, "Emil", 2, 1, 1, 1, "Cuando es invocada busca una trampa en el deck del rival, destierrala", "Dana", "Mercader", 0));
+        cardList.Add(new Card(87, "M.4.1.D", 2, 1, 1, 1, "Cuando es invocada roba una carta de equipo en el deck", "N", "Robot", 0));
+        cardList.Add(new Card(88, "Temp", 1, 1, 1, 1, "Cuando es invocada mira las 3 cartas de la parte superior de tu deck colocalas de vuelta en el orden que quieras", "Chronos", "Explorador", 0));
+        cardList.Add(new Card(89, "Decay", 2, 1, 1, 1, "Cuando es invocada busca una carta de equipo en el deck del rival, destierrala", "Chronos", "Mago", 0));
+        cardList.Add(new Card(90, "Efraim", 2, 1, 1, 1, "Cuando es invocada busca una carta de dominio en el deck del rival, destierrala", "Dana", "Artificiero", 0));
 
+
+        // Miknit's Mana War Legacy 
+
+        cardList.Add(new Card(91, "Myura", 10, 8, 4, 7, "Cuando es inovada limita las zonas de elegidos y cartas especiales a 4, si tu oponente tiene las dos zonas al completo puedes invocar esta carta de la mano", "Miknit", "Maga", 0));
+        cardList.Add(new Card(92, "Etrian", 10, 8, 10, 8, "Invoca tantos elegidos de coste 1 como mana de Miknit tengas, gasta todo el mana de Miknit", "Miknit", "", 0));
+        cardList.Add(new Card(93, "Klio", 10, 10, 8, 8, "Invoca todos los elegidos de tu mano, cuando vayan a ser destruidos destierralos", "Miknit", "", 0));
+
+
+        // Cartas Desbloqueables Mundo Antiguo
+        cardList.Add(new Card(1000, "Blight", 10, 20, 20, 20, "Un rey olvidado por el mundo", "N", "Guerrero", 0));
+        cardList.Add(new Card(1001, "Nº0", 1, 1, 1, 1, "Cuando esta carta es invocada destierra todas las cartas Nº aumenta en 1 su vida por cada una", "N", "Invocadora", 0));
+
+        // Cartas Desbloqueables Revolución Steampunk
+        cardList.Add(new Card(1002, "HelAI", 6, 3, 14, 5, "Si este elegido es el único en tu campo es destruido, si un elegido tuyo tiene una carta de equipo puedes invocar a este de forma especial", "Chronos", "Robot", 0));
 
 
         // Fichas
-        cardList.Add(new Card(0, "Soldado de arena", 0, 0, 4, 1, "No puede ser destruido en batalla si Nº11 esta en el campo", "N", "", 0));
-        cardList.Add(new Card(0, "Chakal", 0, 5, 1, 2, "Al atacar cambia a posicion de defensa", "N", "", 0));
-        cardList.Add(new Card(0, "Lia", 0, 9, 0, 1, "Cuando es destruida en batalla envia del cementerio al deck la carta de equipo Lia", "none", "", 0));
+        cardList.Add(new Card(0, "Soldado de arena", 0, 0, 4, 1, "No puede ser destruido en batalla si Nº11 esta en el campo", "N", "Invocación", 0));
+        cardList.Add(new Card(0, "Chakal", 0, 5, 1, 2, "Al atacar cambia a posicion de defensa", "N", "Invocación", 0));
+        cardList.Add(new Card(0, "Lia", 0, 9, 0, 1, "Cuando es destruida en batalla envia del cementerio al deck la carta de equipo Lia", "none", "Invocación", 0));
     }
 }
