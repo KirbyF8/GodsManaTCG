7a3919603c68846967a3c86c40ca7240f9efbb6e
Héctor <hff150305@gmail.com>
2025-02-16 22:57:42 +0100

    minifix

--- Assets/Scripts/Prologue.cs
+++ Assets/Scripts/Prologue.cs
@@ -54,120 +54,8 @@ public class Prologue : MonoBehaviour
 
     private IEnumerator UpdateText()
     {
-        if (actualText == 0)
-        {
-            background.sprite = backgrounds[0];
-            imageLeft.sprite = truck; //? CAMBIAR
-            imageRight.sprite = godsSprites[3];
-            imageRight.color = Color.gray;
-        }else if (actualText == 1)
-        {
-            imageRight.sprite = godsSprites[4];
-            imageRight.color = Color.white;
-            imageLeft.color = Color.gray;
-        }else if (actualText == 2)
-        {
-            imageLeft.color= Color.white;
-            imageLeft.sprite = godsSprites[2];
-            imageRight.color= Color.gray;
-        }else if (actualText == 3)
-        {
-            imageRight.sprite = godsSprites[1];
-            imageRight.color= Color.white;
-            imageLeft.color= Color.gray;
-        } else if (actualText == 4)
-        {
-            imageLeft.sprite = godsSprites[5];
-            imageLeft.color= Color.white;
-            imageRight.color= Color.gray;
-        } else if (actualText == 5)
-        {
-            imageRight.sprite = truck; //? Cambiar
-            imageRight.color= Color.white;
-            imageLeft.color= Color.gray;
-        } else if (actualText == 6)
-        {
-            imageLeft.sprite = godsSprites[3];
-            imageLeft.color= Color.white;
-            imageRight.color= Color.gray;
-        } else if (actualText == 7)
-        {
-            imageRight.sprite= truck; //? Cambiar
-            imageRight.color= Color.white;
-            imageLeft.color= Color.gray;
-        } else if (actualText == 8)
-        {
-            imageLeft.color = Color.white;
-            imageRight.color= Color.gray;
-        }else if (actualText ==9)
-        {
-            imageLeft.color = Color.gray;
-            imageRight.color= Color.white;
-            imageRight.sprite = godsSprites[5];
-        } else if (actualText == 10)
-        {
-            imageLeft.color = Color.white;
-            imageRight.color= Color.gray;
-        } else if (actualText == 11)
-        {
-            imageLeft.color = Color.gray;
-            imageRight.color= Color.white;
-            imageRight.sprite = godsSprites[1];
-        } else if(actualText == 12)
-        {
-            imageLeft.color = Color.white;
-            imageLeft.sprite = godsSprites[4];
-            imageRight.color= Color.gray;
-        } else if (actualText ==13)
-        {
-            imageLeft.sprite = godsSprites[3];
-        } else if (actualText == 14)
-        {
-            imageLeft.color = Color.gray;
-            imageRight.color= Color.white;
-            imageRight.sprite = godsSprites[2];
-        }else if (actualText == 15)
-        {
-            imageLeft.color = Color.white;
-            imageRight.color= Color.gray;
-        }else if (actualText == 16)
-        {
-            imageLeft.color = Color.gray;
-            imageRight.color= Color.white;
-            imageRight.sprite = truck; //? Cambiar
-        } else if (actualText== 17)
-        {
-            imageLeft.color = Color.white;
-            imageRight.color= Color.gray;
-        }else if (actualText == 18)
-        {
-            imageLeft.color = Color.gray;
-            imageRight.color= Color.white;
-            imageRight.sprite = godsSprites[4];
-        } else if (actualText == 19)
-        {
-            imageLeft.color = Color.white;
-            imageLeft.sprite = godsSprites[5];
-            imageRight.color= Color.gray;
-        }
-        else if (actualText == 20)
-        {
-            imageLeft.color = Color.gray;
-            imageRight.color= Color.white;
-        } else if (actualText == 21)
-        {
-            imageLeft.gameObject.SetActive(false);
-            imageRight.gameObject.SetActive(false);
-            background.sprite = backgrounds[1];
-        }
-        else if (actualText == 22)
-        {
-            imageLeft.gameObject.SetActive(true);
-            imageRight.gameObject.SetActive(true);
-        }
 
-       
-
+        UpdateImages();
         runningText = true;
         textBox.text = "";
         yield return new WaitForSeconds(0.5f);
@@ -191,5 +79,140 @@ public class Prologue : MonoBehaviour
         textBox.text = dialogues[actualText];
     }
    
+    private void UpdateImages()
+    {
+        if (actualText == 0)
+        {
+            background.sprite = backgrounds[0];
+            imageLeft.sprite = truck; //? CAMBIAR
+            imageRight.sprite = godsSprites[3];
+            imageRight.color = Color.gray;
+        }
+        else if (actualText == 1)
+        {
+            imageRight.sprite = godsSprites[4];
+            imageRight.color = Color.white;
+            imageLeft.color = Color.gray;
+        }
+        else if (actualText == 2)
+        {
+            imageLeft.color = Color.white;
+            imageLeft.sprite = godsSprites[2];
+            imageRight.color = Color.gray;
+        }
+        else if (actualText == 3)
+        {
+            imageRight.sprite = godsSprites[1];
+            imageRight.color = Color.white;
+            imageLeft.color = Color.gray;
+        }
+        else if (actualText == 4)
+        {
+            imageLeft.sprite = godsSprites[5];
+            imageLeft.color = Color.white;
+            imageRight.color = Color.gray;
+        }
+        else if (actualText == 5)
+        {
+            imageRight.sprite = truck; //? Cambiar
+            imageRight.color = Color.white;
+            imageLeft.color = Color.gray;
+        }
+        else if (actualText == 6)
+        {
+            imageLeft.sprite = godsSprites[3];
+            imageLeft.color = Color.white;
+            imageRight.color = Color.gray;
+        }
+        else if (actualText == 7)
+        {
+            imageRight.sprite = truck; //? Cambiar
+            imageRight.color = Color.white;
+            imageLeft.color = Color.gray;
+        }
+        else if (actualText == 8)
+        {
+            imageLeft.color = Color.white;
+            imageRight.color = Color.gray;
+        }
+        else if (actualText == 9)
+        {
+            imageLeft.color = Color.gray;
+            imageRight.color = Color.white;
+            imageRight.sprite = godsSprites[5];
+        }
+        else if (actualText == 10)
+        {
+            imageLeft.color = Color.white;
+            imageRight.color = Color.gray;
+        }
+        else if (actualText == 11)
+        {
+            imageLeft.color = Color.gray;
+            imageRight.color = Color.white;
+            imageRight.sprite = godsSprites[1];
+        }
+        else if (actualText == 12)
+        {
+            imageLeft.color = Color.white;
+            imageLeft.sprite = godsSprites[4];
+            imageRight.color = Color.gray;
+        }
+        else if (actualText == 13)
+        {
+            imageLeft.sprite = godsSprites[3];
+        }
+        else if (actualText == 14)
+        {
+            imageLeft.color = Color.gray;
+            imageRight.color = Color.white;
+            imageRight.sprite = godsSprites[2];
+        }
+        else if (actualText == 15)
+        {
+            imageLeft.color = Color.white;
+            imageRight.color = Color.gray;
+        }
+        else if (actualText == 16)
+        {
+            imageLeft.color = Color.gray;
+            imageRight.color = Color.white;
+            imageRight.sprite = truck; //? Cambiar
+        }
+        else if (actualText == 17)
+        {
+            imageLeft.color = Color.white;
+            imageRight.color = Color.gray;
+        }
+        else if (actualText == 18)
+        {
+            imageLeft.color = Color.gray;
+            imageRight.color = Color.white;
+            imageRight.sprite = godsSprites[4];
+        }
+        else if (actualText == 19)
+        {
+            imageLeft.color = Color.white;
+            imageLeft.sprite = godsSprites[5];
+            imageRight.color = Color.gray;
+        }
+        else if (actualText == 20)
+        {
+            imageLeft.color = Color.gray;
+            imageRight.color = Color.white;
+        }
+        else if (actualText == 21)
+        {
+            imageLeft.gameObject.SetActive(false);
+            imageRight.gameObject.SetActive(false);
+            background.sprite = backgrounds[1];
+        }
+        else if (actualText == 22)
+        {
+            imageLeft.gameObject.SetActive(true);
+            imageRight.gameObject.SetActive(true);
+        }
 
+
+    }
 }
